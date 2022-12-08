b9410605c9608fc37f6ec64f916df1a919d617e1
leesuen <suen@mkcreative.co.kr>
2022-12-08 16:48:14 +0900

    일기 수정 기능 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import {useRef, useState, useContext} from 'react';
+import {useRef, useState, useContext,useEffect} from 'react';
 import {DiaryDispatchContext} from './../App.js';
 
 import MyHeader from "./MyHeader";
@@ -53,13 +53,13 @@ const getStringDate = (date) => {
     return `${year}-${month}-${day}`;
 }
 
-const DiaryEditor = () => {
+const DiaryEditor = ({isEdit, originData}) => {
     const contentRef = useRef();
     const [content, setContent] = useState("");
     const [emotion, setEmotion] = useState(3);
     const [date, setDate] = useState(getStringDate(new Date()));
 
-    const {onCreate} = useContext(DiaryDispatchContext);
+    const {onCreate, onEdit} = useContext(DiaryDispatchContext);
 
     const handleClickEmote = (emotion) => {
         setEmotion(emotion);
@@ -71,16 +71,32 @@ const DiaryEditor = () => {
             return
         }
 
+        if (window.confirm(isEdit? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?")) {
+            if (!isEdit) {
+                onCreate(date, content, emotion);
+            } else {
+                onEdit(originData.id, date, content, emotion);
+            }
+        }
+
         onCreate(date, content, emotion);
         navigate('/', {replace:true})
     }
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        if(isEdit) {
+            setDate(getStringDate(new Date(parseInt(originData.date))));
+            setEmotion(originData.emotion);
+            setContent(originData.content);
+        }
+    }, [isEdit, originData])
+
     return (
         <div className="DiaryEditor">
             <MyHeader 
-                headTitle={"새 일기쓰기"}
+                headTitle={isEdit ? "일기 수정하기" : "새 일기쓰기"}
                 headLeftChild={
                     <MyButton 
                         text={<i className="fa-solid fa-chevron-left"></i>}
